739956169cfcfacb3dd47bb6b278adb8025dd233
Alexis Romero Mendoza <alexis.rinnegan@gmail.com>
2025-10-24 09:00:39 -0600

    Update page.tsx

--- app/(private)/crear-producto/page.tsx
+++ app/(private)/crear-producto/page.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const CrearProductoPage = () => {
   return (
-    <div>CrearProductoPage</div>
+    <div>
+      <h1>Crear nuevo producto</h1>
+      <form className='w-full max-w-lg'>
+        <div>
+          <label htmlFor="nombre-producto">Nombre del producto:</label>
+          <input type="text"  id="nombre-prducto"/>
+        </div>
+      </form>
+    </div>
   )
 }
 
